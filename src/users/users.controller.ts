973d6ab9b4a6b040bdecfeeb025f512ea330949c
mohammaduamhar <mamamhar254@gmail.com>
2024-06-15 23:37:21 +0530

    modify Controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,32 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+
+   
+
+    @Get()
+    findByRole(@Query('role') role?: 'intern' | 'Admin'){
+        return [];
+    }
+
+    @Get(':id')
+    findOne(@Param('id') id : string) {
+        return {id} ;
+    }
+
+    @Post()
+    create(@Body() user : {}) {   
+        return user;
+    }
+
+    @Patch(':id')
+    update(@Param('id') id : string, @Body() updateUser : {}) {
+        return {id, ...updateUser} ;
+    }
+
+    @Delete(':id')
+    remove(@Param('id') id : string) {
+        return {id};
+    }
+}
